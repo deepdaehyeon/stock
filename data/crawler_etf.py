a3ea7ba726d928910ca89ea7c71ee80a7ca3dea4
Hongkyu Kim <kimaa112@gmail.com>
2022-11-19 21:57:30 +0900

    MP-9 ETF Crawling Module 개발 * main.py: Module 불러와서 실행 * crawler_etf.py: config에서 ETF List 불러와서, 지정한 기간동안 데이터 모두 Crawling (ETF 개수 * 기간(연))

--- data/crawler_etf.py
+++ data/crawler_etf.py
@@ -1,39 +1,32 @@
+from datetime import datetime
+import os
+import yaml
+import yfinance as yf
 
-# Libraries
-if True: 
-    from datetime import datetime
-    import yaml
-    import os
-    import argparse 
-    import yfinance as yf 
 
-# Config & Args 
-if True: 
-    parser = argparse.ArgumentParser() 
-    parser.add_argument('--year', type=int, default= 2022)
-    args = parser.parse_args()
-    BASEPATH =os.getcwd() +'/..' 
-    with open(BASEPATH+'/config/config.yaml') as conf:
-        config = yaml.load(conf, Loader = yaml.FullLoader) 
+class EtfCrawler:
+    def __init__(self, export_dir='data/rawdata'):
+        self.BASEPATH = os.getcwd().replace('\\', '/')
+        self.export_dir = export_dir
 
-# Main
-def main(): 
-    # Set date 
-    from_date = f'{args.year}-01-01'
-    if args.year == 2022: 
-        to_date = datetime.now().strftime('%Y-%m-%d') 
-    else: 
-        to_date = f'{args.year}-12-31'
+    def crawl(self, ticker_list: list = None, from_year: int = 2000, to_year: int = 2022):
+        if ticker_list is None:
+            with open(f'{self.BASEPATH}/config/config.yaml') as conf:
+                config = yaml.load(conf, Loader=yaml.FullLoader)
+                ticker_list = config['etf']
 
-    # Data crawling
-    for ticker in config['etf']: 
-        print(f'{ticker}')
-        df = yf.download(ticker, start = from_date, end = to_date)
+        for year in range(from_year, to_year+1):
+            # Set date
+            from_date = f'{year}-01-01'
+            if year == int(datetime.now().strftime('%Y')):
+                to_date = datetime.now().strftime('%Y-%m-%d')
+            else:
+                to_date = f'{year}-12-31'
 
-        df.to_csv(BASEPATH+f'/data/rawdata/{ticker}_{args.year}.csv')
-
-if __name__ =='__main__': 
-    main()     
+            # Data Crawling
+            for ticker in ticker_list:
+                df = yf.download(ticker, start=from_date, end=to_date)
+                df.to_csv(f'{self.BASEPATH}/{self.export_dir}/{ticker}_{year}.csv')
     
 
  
---
+++ main.py
@@ -0,0 +1,5 @@
+from data.crawler_etf import EtfCrawler
+
+
+crawler = EtfCrawler()
+crawler.crawl()
