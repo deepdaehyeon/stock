909278b996e8baa14299bbc9d7e9368a3dcaa9c3
Hongkyu Kim <kimaa112@gmail.com>
2022-12-04 22:16:45 +0900

    MP-13 Add Feature Next Close Price ("next_close") for Calculating Reward

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -11,13 +11,13 @@ class Prep:
         self.args = args 
         
     def _prep(self): 
-        DATAPATH =self.BASEPATH + self.export_dir
+        DATAPATH = self.BASEPATH + self.export_dir
         ret = None 
         # for all raw data files
         for f in os.listdir(DATAPATH): 
             # Load data
             df = pd.read_csv(DATAPATH+f)
-            df['Date']= df.Date.transform(lambda x: x.split(' ')[0])
+            df['Date'] = df.Date.transform(lambda x: x.split(' ')[0])
             
             # Feature engineering
             L = self.args.long_period
@@ -38,14 +38,17 @@ class Prep:
             df['Lvolume'] = df['Volume'].rolling(L).mean()
             df['Mvolume'] = df['Volume'].rolling(M).mean()
             df['Svolume'] = df['Volume'].rolling(S).mean()
+
+            ## Reward 계산 목적 다음 날 종가 가격 저장 (리밸런싱 시점: 종가 -> 종가에 사고팔고, 다음날은 종가까지 기다림)
+            df['next_close_price'] = df['Close'].shift(periods=-1)
             
             ## Price scaling  
             cols = [c for c in df.columns if 'Date' not in c]  
             cols_price = [c for c in cols if 'amount' not in c]  
-            if self.args.scaling_mode =='plain':
+            if self.args.scaling_mode == 'plain':
                 den = df[self.args.scaling_price]
                 mean = 0 
-            elif self.args.scaling_mode =='minmax':
+            elif self.args.scaling_mode == 'minmax':
                 mean = df[self.args.scaling_price[0]]
                 den = df[self.args.scaling_price[1]] - mean
             else:
@@ -65,14 +68,14 @@ class Prep:
 
             # rename column 
             D = {c:c+"_"+f.split('.')[0] for c in df.columns if 'Date' not in c}
-            df.rename(D, axis=1, inplace= True)
+            df.rename(D, axis=1, inplace=True)
             
             # Merge
             
             if ret is None: 
                 ret = df 
             else: 
-                ret = pd.merge(left = ret, right = df, on='Date', validate='one_to_one') 
+                ret = pd.merge(left=ret, right=df, on='Date', validate='one_to_one')
         
         # Append day and month 
         ret['day'] = ret['Date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').weekday()) 
@@ -90,11 +93,3 @@ class Prep:
         df_train.to_csv(DATAPATH+f'../train.csv')
         df_valid.to_csv(DATAPATH+f'../valid.csv')
         df_test.to_csv(DATAPATH+f'../test.csv')
-
-
-
-
-     
-    
-
- 
