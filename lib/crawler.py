e8c4f18b58b79f06a095f2905d9d5dc7f02c77e0
deepdaehyeon <kimdhyeon@gmail.com>
2023-02-02 22:06:00 +0900

    FEAT: listup top 100 etf list

--- lib/crawler.py
+++ lib/crawler.py
@@ -11,26 +11,31 @@ C = Config
 # Modules
 @dataclass
 class Crawler:
-    from_date: str 
+    from_date: str = '2020-01-01' 
     to_date: datetime = field(default_factory = lambda: datetime.now().strftime('%Y-%m-%d'))  
 
     def __post_init__(self): 
         assert '-' in self.from_date, 'date format is 20xx-xx-xx' 
         assert '-' in self.to_date, 'date format is 20xx-xx-xx' 
         
-    def run_etf(self, ticker_list):
+    def etf_list(self) : 
+        pass 
+    
+    def crawling_etf(self, ticker_list, from_date=None, to_date=None):
+        if from_date is not None: 
+            self.from_date = from_date
+        if to_date is not None: 
+            self.to_date = to_date
+            
         for ticker in ticker_list:
             df = yf.download(ticker, start=self.from_date, end=self.to_date)
             df.to_csv(os.path.join(C.datapath, 'rawdata', ticker+'.csv'))
+            
 
-    def run_indicator(self, indicator_list): 
-        NotImplemented() 
-        # Economic indicator 
-        # if index_list is None: 
-        #     ticker_list = self.config['index']
-        
+    def crawling_macro(self, indicator_list): 
         '''
         macro economic indicator 의 경우, 
+        # Economic indicator 
         아래의 사이트 중에서 직접 크롤링을 통해 구해야해서 좀 까다로움 
         Phase 2. 에서 진행하는게 나을듯  
 
@@ -45,6 +50,8 @@ class Crawler:
         - https://fred.stlouisfed.org/series/T10Y2Y
         - www.currentmarketevaluation.com 
         '''
+        pass
+        
     
 
  
